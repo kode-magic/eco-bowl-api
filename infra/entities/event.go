4ddbad54a4a4f632776bebd34b25f96212477883
kode-magic <mohamedjramsey@gmail.com>
2022-10-28 14:22:36 +0000

    Create event.go
    infra event

---
+++ infra/entities/event.go
@@ -0,0 +1,35 @@
+package entities
+
+import (
+	"github.com/google/uuid"
+	"github.com/kode-magic/go-bowl/ulids"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Event struct {
+	ID            uuid.UUID   `gorm:"type:uuid;"`
+	Name          string      `gorm:"not null;"`
+	Description   string      `gorm:"not null;"`
+	StartDate     time.Time   `gorm:"not null;"`
+	EndDate       time.Time   `gorm:"not null;"`
+	InstitutionID string      `gorm:"not null;"`
+	Institution   Institution `json:"institution"`
+	CreatedAt     time.Time   `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt     time.Time   `gorm:"default:CURRENT_TIMESTAMP"`
+}
+
+func (e *Event) BeforeCreate(_ *gorm.DB) error {
+	e.ID = ulids.GenerateUUID()
+	return nil
+}
+
+func (e *Event) BeforeUpdate(_ *gorm.DB) error {
+	e.UpdatedAt = time.Now()
+	return nil
+}
+
+func (e *Event) Prepare() {
+	e.CreatedAt = time.Now()
+	e.UpdatedAt = time.Now()
+}
