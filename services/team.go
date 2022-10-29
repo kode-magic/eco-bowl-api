1ced3e31363219fe1d92fd1b9c54c9aca92fdca2
kode-magic <mohamedjramsey@gmail.com>
2022-10-29 04:33:14 +0000

    Update team.go
    create teams and add members to team

--- services/team.go
+++ services/team.go
@@ -5,27 +5,54 @@ import (
 	core "github.com/kode-magic/eco-bowl-api/core/entities"
 )
 
+type TeamRequest struct {
+	Name        string   `json:"name" binding:"required"`
+	Description string   `json:"description" binding:"required"`
+	Event       string   `json:"event"`
+	Members     []string `json:"members" binding:"required"`
+}
+
 type TeamService struct {
-	Team        core.Team
-	Trainee     core.Trainee
 	Repo        core.TeamRepo
 	TraineeRepo core.TraineeRepo
 }
 
-func (t TeamService) Create(team *core.Team) (*core.Team, map[string]string) {
-	_, err := t.Repo.GetByName(team.EventID, team.Name)
+func (t TeamService) Create(param *TeamRequest) (*core.Team, map[string]string) {
+	_, err := t.Repo.GetByName(param.Event, param.Name)
 
 	if err != nil {
-		_, teamErr := t.Repo.Create(team)
+		teamCreate := core.Team{
+			Name:        param.Name,
+			Description: param.Description,
+			EventID:     param.Event,
+			Event: core.Event{
+				ID: param.Event,
+			},
+		}
+		team, teamErr := t.Repo.Create(&teamCreate)
 		if teamErr != nil {
 			return nil, teamErr
 		}
 
-		return t.Repo.Create(team)
+		members := make([]core.Trainee, len(param.Members))
+
+		for i, member := range param.Members {
+			addTrainees := core.Trainee{
+				ID:     member,
+				TeamID: team.ID,
+			}
+
+			trainee, _ := t.TraineeRepo.AddToTeam(&addTrainees)
+			members[i] = *trainee
+		}
+
+		team.Trainees = members
+
+		return team, nil
 	}
 
 	errMessage := make(map[string]string)
-	errMessage["name"] = errors.New("team with name " + team.Name + " already exist").Error()
+	errMessage["name"] = errors.New("team with name " + param.Name + " already exist").Error()
 
 	return nil, errMessage
 }
