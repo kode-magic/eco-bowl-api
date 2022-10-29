b2fe26f0e6915460e6e9a8627519971615df7742
kode-magic <mohamedjramsey@gmail.com>
2022-10-29 03:19:29 +0000

    Update team.go
    team service

--- services/team.go
+++ services/team.go
@@ -1,7 +1,31 @@
 package services
 
-import core "github.com/kode-magic/eco-bowl-api/core/entities"
+import (
+	"errors"
+	core "github.com/kode-magic/eco-bowl-api/core/entities"
+)
 
 type TeamService struct {
-	Repo core.TeamRepo
+	Team        core.Team
+	Trainee     core.Trainee
+	Repo        core.TeamRepo
+	TraineeRepo core.TraineeRepo
+}
+
+func (t TeamService) Create(team *core.Team) (*core.Team, map[string]string) {
+	_, err := t.Repo.GetByName(team.EventID, team.Name)
+
+	if err != nil {
+		_, teamErr := t.Repo.Create(team)
+		if teamErr != nil {
+			return nil, teamErr
+		}
+
+		return t.Repo.Create(team)
+	}
+
+	errMessage := make(map[string]string)
+	errMessage["name"] = errors.New("team with name " + team.Name + " already exist").Error()
+
+	return nil, errMessage
 }
