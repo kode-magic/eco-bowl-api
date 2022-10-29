8cf3dcac30648f6b381f86db750678cc9287b2d7
kode-magic <mohamedjramsey@gmail.com>
2022-10-29 05:42:27 +0000

    Create solution.go
    infra solution

---
+++ infra/entities/solution.go
@@ -0,0 +1,35 @@
+package entities
+
+import (
+	"github.com/google/uuid"
+	"github.com/kode-magic/go-bowl/ulids"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Solution struct {
+	ID          uuid.UUID `gorm:"type:uuid;"`
+	Title       string    `gorm:"not null;"`
+	Description string    `gorm:"default:null;"`
+	EventID     string    `gorm:"not null;"`
+	Event       Event
+	TeamID      string `gorm:"not null;"`
+	Team        Team
+	CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP"`
+}
+
+func (r *Solution) BeforeCreate(_ *gorm.DB) error {
+	r.ID = ulids.GenerateUUID()
+	return nil
+}
+
+func (r *Solution) BeforeUpdate(_ *gorm.DB) error {
+	r.UpdatedAt = time.Now()
+	return nil
+}
+
+func (r *Solution) Prepare() {
+	r.CreatedAt = time.Now()
+	r.UpdatedAt = time.Now()
+}
