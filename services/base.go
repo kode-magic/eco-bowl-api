39d038352e0f9d52439246a45a752c0f210807c6
kode-magic <mohamedjramsey@gmail.com>
2022-10-29 11:51:04 +0000

    Update base.go
    base update

--- services/base.go
+++ services/base.go
@@ -3,11 +3,12 @@ package services
 import core "github.com/kode-magic/eco-bowl-api/core/entities"
 
 type BaseService struct {
-	User      core.UserRepository
-	Institute core.InstitutionRepo
-	Event     core.EventRepo
-	Reward    core.RewardRepo
-	Team      core.TeamRepo
-	Trainee   core.TraineeRepo
-	Solution  core.SolutionRepo
+	User         core.UserRepository
+	Institute    core.InstitutionRepo
+	Event        core.EventRepo
+	Reward       core.RewardRepo
+	Team         core.TeamRepo
+	Trainee      core.TraineeRepo
+	Solution     core.SolutionRepo
+	Entrepreneur core.EntrepreneurRepo
 }
